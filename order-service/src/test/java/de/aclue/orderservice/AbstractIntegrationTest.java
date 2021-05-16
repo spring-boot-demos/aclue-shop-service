243ed94d9d57149a0b0a33bb8571229675453c35
Jonas Keßler <jonas.kessler@aclue.de>
2021-05-16 20:19:32 +0200

    use wiremock to mock warehouse-service

--- order-service/src/test/java/de/aclue/orderservice/AbstractIntegrationTest.java
+++ order-service/src/test/java/de/aclue/orderservice/AbstractIntegrationTest.java
@@ -1,12 +1,17 @@
 package de.aclue.orderservice;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.WireMockServer;
 
 import de.aclue.orderservice.persistence.repostiory.OrderRepository;
 
@@ -17,15 +22,32 @@ import de.aclue.orderservice.persistence.repostiory.OrderRepository;
 @SpringBootTest
 @ActiveProfiles("integrationtest")
 @AutoConfigureMockMvc
+@Import(TestConfig.class)
 public abstract class AbstractIntegrationTest {
 
 	@Autowired
 	protected MockMvc mockMvc;
-	
+
 	@Autowired
 	protected ObjectMapper objectMapper;
-	
+
 	@Autowired
 	protected OrderRepository orderRepository;
-	
+
+	@Autowired
+	private WireMockServer wireMockServer;
+
+	@AfterEach
+	public void teardown() {
+
+		orderRepository.deleteAll();
+
+		try {
+			assertThat(wireMockServer.findUnmatchedRequests().getRequests()).hasSize(0);
+		} finally {
+			wireMockServer.resetRequests();
+			wireMockServer.resetToDefaultMappings();
+		}
+	}
+
 }
